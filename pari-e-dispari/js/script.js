42a2a4e4004c997c3e006dc2ce3904e1639dfa64
LarisaGimbu <gimbularisa@gmail.com>
2021-10-29 15:27:25 +0200

    pari o dispari function

--- pari-e-dispari/js/script.js
+++ pari-e-dispari/js/script.js
@@ -6,14 +6,28 @@
   Dichiariamo chi ha vinto.
 */
 
+const userChoice = 'pari';
 const userNumber = 3; //prompt
+const text = document.getElementById('text');
 
 let computerNumber = getRandomNumber(1, 5);
-console.log(computerNumber);
+// console.log(computerNumber);
 
 function getRandomNumber (min, max){
   return Math.floor(Math.random()*(max - min +1) +min);
 }
 
 let somma = userNumber + computerNumber;
-console.log(somma);
+// console.log(somma);
+
+
+let output = pariODispari(somma);
+text.innerHTML = output;
+
+function pariODispari (numero){
+  if(((numero % 2) && (userChoice==='dispari')) || ((numero % 2 === 0) && (userChoice === 'pari'))) {
+    return `<h3>Hai Vinto!</h3> Hai scelto <strong>${userChoice}</strong>, il tuo numero è <strong>${userNumber}</strong> mentre quello del computer è <strong>${computerNumber}</strong>. La somma dei vostri numeri è <strong>${somma}</strong>. `;
+  }
+
+  return `<h3>Hai Perso!</h3> Hai scelto <strong>${userChoice}</strong>, il tuo numero è <strong>${userNumber}</strong> mentre quello del computer è <strong>${computerNumber}</strong>. La somma dei vostri numeri è <strong>${somma}</strong>. Hai Perso!`;
+}
