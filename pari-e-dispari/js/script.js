71de16d6b051a63bb757a010bf0ddac2222c81ba
LarisaGimbu <gimbularisa@gmail.com>
2021-10-29 16:05:30 +0200

    errorMsg

--- pari-e-dispari/js/script.js
+++ pari-e-dispari/js/script.js
@@ -6,8 +6,8 @@
   Dichiariamo chi ha vinto.
 */
 
-const userChoice = 'pari';
-const userNumber = 3; //prompt
+const userChoice = prompt('Scegliere fra pari o dispari')
+const userNumber = parseInt(prompt('Inserire un numero compreso tra 1 e 5')) ;
 const text = document.getElementById('text');
 
 let computerNumber = getRandomNumber(1, 5);
@@ -20,14 +20,35 @@ function getRandomNumber (min, max){
 let somma = userNumber + computerNumber;
 // console.log(somma);
 
-
+let formValido = true;
 let output = pariODispari(somma);
-text.innerHTML = output;
+let errorMsg = '';
+
+
+if((userChoice != 'pari') && (userChoice != 'dispari')){
+  formValido = false;
+  errorMsg = 'Deve inserire "pari" o "dispari"';
+}
+if(isNaN(userNumber)){
+  formValido = false
+  errorMsg = 'Deve inserire un numero';
+}
+if((userNumber > 5) || (userNumber < 0)){
+  formValido= false
+  errorMsg = 'Deve inserie un numero compreso fra 1 e 5';
+}
+
+if(formValido){
+  text.innerHTML = output;
+}else{
+  text.innerHTML = errorMsg;
+}
+
 
 function pariODispari (numero){
   if(((numero % 2) && (userChoice==='dispari')) || ((numero % 2 === 0) && (userChoice === 'pari'))) {
     return `<h3>Hai Vinto!</h3> Hai scelto <strong>${userChoice}</strong>, il tuo numero è <strong>${userNumber}</strong> mentre quello del computer è <strong>${computerNumber}</strong>. La somma dei vostri numeri è <strong>${somma}</strong>. `;
   }
 
-  return `<h3>Hai Perso!</h3> Hai scelto <strong>${userChoice}</strong>, il tuo numero è <strong>${userNumber}</strong> mentre quello del computer è <strong>${computerNumber}</strong>. La somma dei vostri numeri è <strong>${somma}</strong>. Hai Perso!`;
+  return `<h3>Hai Perso!</h3> Hai scelto <strong>${userChoice}</strong>, il tuo numero è <strong>${userNumber}</strong> mentre quello del computer è <strong>${computerNumber}</strong>. La somma dei vostri numeri è <strong>${somma}</strong>.`;
 }
